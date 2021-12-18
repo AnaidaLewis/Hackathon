a05de73245cc7c32a9199f264685ab35103060c2
PRACHIP09 <prachipp999@gmail.com>
2021-12-18 16:30:16 +0530

    updated dashboard

--- hackathon/src/App.js
+++ hackathon/src/App.js
@@ -12,25 +12,18 @@ import Feedback from './Pages/Feedback';
 import HeaderFrontPage from './HeaderFooter/HeaderFrontPage';
 import { Navbar } from './Pages/Navbar';
 import NotFound from './notFound/NotFound';
-import Dashboard from './Homepage_bigCompany/Dashboard';
+import Dashboard from './Homepage_smallCompany/Dashboard';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
         <Route exact path = "/" element={<div className='sign'><HeaderFrontPage/><Signin/></div>}/>
-        <Route path = "Login" element={<div className='sign'><HeaderFrontPage/><Login/></div>} /
-        <Route path = "Login/HomePage" element={<><Navbar/><HomePage/></>} />
+        <Route path = "Login" element={<div className='sign'><HeaderFrontPage/><Login/></div>} />
+        <Route path = "Login/HomePage" element={<><Navbar/><HomeC/></>} />
         <Route path = "/Page1" element={<><Navbar/><Page1/></>}/>
         <Route path = "/Page2" element={<><Navbar/><Page2/></>} />
         {/*<Route path = "/Feedback" element={<><Navbar/><Feedback/></>} />*/}
-        <Route path ='/Dashboard' element={<><Navbar /><Dashboard/></>} />
-=======
-        <Route path = "login/HomePage" element={<><Sidebar/><Navbar/><HomeC/></>} />
-        <Route path = "/Page1" element={<><Sidebar/><Navbar/><Page1/></>}/>
-        <Route path = "/Page2" element={<><Sidebar/><Navbar/><Page2/></>} />
-        <Route path = "/Feedback" element={<><Sidebar/><Navbar/><Feedback/></>} />
->>>>>>> 7022cf824e32524786b98337f3ec9d4693837373
         <Route element={<h1>not found</h1>} />
         <Route path="*" element={<NotFound/>} />
         </Routes>
