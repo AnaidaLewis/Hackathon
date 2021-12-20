63626bc409024afc28ed040d47198e4160dc6a0c
AnaidaLewis <anaidallewis@gmail.com>
2021-12-20 16:37:39 +0530

    history url

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import AddressList, ProductList,BuyerCart,PlaceOrder, SellerViewOrder,AllProducts
+from .views import AddressList, ProductList,BuyerCart,PlaceOrder, SellerViewOrder,AllProducts,BuyerHistory
 urlpatterns = [
     path('address/', AddressList.as_view(), name="Address"),
     path('all-product/',AllProducts.as_view(), name="AllProduct"),
     path('product/<int:pk>/', ProductList.as_view(), name="Product"),
     path('cart/<int:pk>/', BuyerCart.as_view(), name="BuyerCart"),
+    path('cart-history/', BuyerHistory.as_view(), name="BuyerHistory"),
     path('place-order/', PlaceOrder.as_view(), name="BuyerCart"),
     path('seller-view-order/<int:pk>/', SellerViewOrder.as_view(), name="SellerViewOrder"),
 ]
